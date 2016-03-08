9c6ee5252d84b80bce01acd2c884c5d56d7493c1
Patrick Sobolewski <psobolew@mymail.mines.edu>
2016-03-08 00:12:19 -0700

    Implemented a working color wheel.

--- PathDraw/MainPage.xaml.cs
+++ PathDraw/MainPage.xaml.cs
@@ -37,15 +37,80 @@ namespace PathDraw
             this.InitializeComponent();
         }
 
-        private void setupRobot()
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            SetupRobotConnection();
+            Application app = Application.Current;
+            app.Suspending += OnSuspending;
+        }
+
+        /*!
+         * @brief   handle the user launching this page in the application
+         * 
+         *  connects to sphero and sets up the ui
+         */
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            ShutdownRobotConnection();
+            ShutdownControls();
+
+            Application app = Application.Current;
+            app.Suspending -= OnSuspending;
+        }
+
+        //! @brief  handle the application entering the background
+        private void OnSuspending(object sender, SuspendingEventArgs args)
+        {
+            ShutdownRobotConnection();
+        }
+
+        //  just color wheel for now
+        private void SetupControls()
+        {
+            m_colorwheel = new ColorWheel(ColorPuck, m_robot);
+        }
+
+        //! @brief  shuts down the various sphero controls
+        private void ShutdownControls()
+        {
+            m_colorwheel = null;
+        }
+
+        //! @brief  search for a robot to connect to
+        private void SetupRobotConnection()
         {
             RobotProvider provider = RobotProvider.GetSharedProvider();
             provider.DiscoveredRobotEvent += OnRobotDiscovered;
             provider.NoRobotsEvent += OnNoRobotsEvent;
-            //provider.ConnectedRobotEvent += OnRobotConnected;
+            provider.ConnectedRobotEvent += OnRobotConnected;
             provider.FindRobots();
         }
 
+        //! @brief  disconnect from the robot and stop listening
+        private void ShutdownRobotConnection()
+        {
+            if (m_robot != null)
+            {
+                m_robot.SensorControl.StopAll();
+                m_robot.Sleep();
+
+                RobotProvider provider = RobotProvider.GetSharedProvider();
+                provider.DiscoveredRobotEvent -= OnRobotDiscovered;
+                provider.NoRobotsEvent -= OnNoRobotsEvent;
+                provider.ConnectedRobotEvent -= OnRobotConnected;
+            }
+        }
+
+        //! @brief  when a robot is discovered, connect!
         private void OnRobotDiscovered(object sender, Robot robot)
         {
             Debug.WriteLine(string.Format("Discovered \"{0}\"", robot.BluetoothName));
@@ -54,8 +119,22 @@ namespace PathDraw
             {
                 RobotProvider provider = RobotProvider.GetSharedProvider();
                 provider.ConnectRobot(robot);
-                //...
+                m_robot = (Sphero)robot;
             }
+        }
+
+        //! @brief  when a robot is connected, get ready to drive!
+        private void OnRobotConnected(object sender, Robot robot)
+        {
+            Debug.WriteLine(string.Format("Connected to {0}", robot));
+
+            changeColor(255, 255, 255);
+            SetupControls();
+
+            // these two are probably useless for my app
+            m_robot.SensorControl.Hz = 10;
+            m_robot.CollisionControl.StartDetectionForWallCollisions();
+
         }
 
         private void OnNoRobotsEvent(object sender, EventArgs e)
@@ -64,7 +143,7 @@ namespace PathDraw
             //...
         }
 
-        public void changeSpheroColor(int red, int green, int blue)
+        public void changeColor(int red, int green, int blue)
         {
             m_robot.SetRGBLED(red, green, blue);
         }
